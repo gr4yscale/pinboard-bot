9a83161ae4e8df6227a2ba3faddc8f8694a5a95e
Tyler Powers <gr4yscale@gmail.com>
2015-07-17 00:31:14 +0100

    find a random post which does not have the hasBeenTweeted key, by tag

--- pinboard-datastore.js
+++ pinboard-datastore.js
@@ -28,4 +28,18 @@ PinboardDataStore.prototype.updateWithPosts = function(posts) {
   });
 };
 
+PinboardDataStore.prototype.findPostWhichHasNotBeenTweetedWithTag = function(tag, callback) {
+  connect(function(db) {
+    var collection = db.collection('posts');
+    // posts which have not been tweeted do not have the 'hasBeenTweeted' key
+    collection.find({hasBeenTweeted:{$exists: false}, tagsArray:tag})
+              .toArray(function(err, objects) {
+                if (err) { console.log(err); return; } // handle better in the future
+                var randIndex = Math.floor(Math.random() * objects.length);
+                if (callback) callback(objects[randIndex]);
+                db.close();
+              });
+  });
+};
+
 module.exports = new PinboardDataStore();
