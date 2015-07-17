0fedf3995c7a2f55b98f6b4d536dbcc044f44f16
Tyler Powers <gr4yscale@gmail.com>
2015-07-17 01:04:44 +0100

    begin making some tweeting random posts by tag, with 140 character limit in mind

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ require('dotenv').load();
 var Twit = require('twit');
 var Bitly = require('bitly');
 var fetchPinboardData = require('./fetch-pinboard-data.js');
+var pinboardDataStore = require('./pinboard-datastore.js');
 
 // config vars (cleanup later)
 
 var pinboardFetchInterval = 1000 * 60 * 60; // every hour
-
-
+var tweetInterval = 1000 * 60 * 30; // every 30 mins
+var tweetIntervalVariance =  1000 * 60 * 5; // 5 mins
+var currentTagToPost = 'techart';
 
 var T = new Twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -21,21 +23,49 @@ var T = new Twit({
 
 var bitly = new Bitly('gr4yscalebitly', process.env.BITLY_API_KEY);
 
-
-
-//T.post('statuses/update', { status: '*yawn*' }, function(err, data, response){
-  //console.log(data);
-//});
-
-bitly.shorten('https://github.com/gr4yscale', function(err, response) {
-  if (err) throw err;
-  var shortenedurl = response.data.url;
-  console.log(shortenedurl);
-});
-
 setInterval(function() {
   console.log('Fetching pinboard data');
-  fetchPinboardData();
+  fetchPinboardDataAndUpdateDataStore();
 }, pinboardFetchInterval);
 
-fetchPinboardData();
+fetchPinboardDataAndUpdateDataStore();
+tweetRandomPostForCurrentTag();
+
+function fetchPinboardDataAndUpdateDataStore() {
+  fetchPinboardData(function(posts){
+    pinboardDataStore.updateWithPosts(posts);
+  });
+}
+
+function formattedTweetWithPostAndURL(post, url) {
+  var tweet = '';
+  var shortenedurl = url;
+  var truncatedTitle = post.description.substring(0, 137 - shortenedurl.length); // 137 to make room for hyphens and a space
+  // do we need to truncate more for ellipses?
+  if (truncatedTitle.length > (134 - shortenedurl.length)) {
+    tweet = truncatedTitle + '... - ' + shortenedurl;
+  } else {
+    tweet = truncatedTitle + ' - ' + shortenedurl;
+  }
+  return tweet;
+}
+
+function tweetRandomPostForCurrentTag() {
+  pinboardDataStore.findPostWhichHasNotBeenTweetedWithTag(currentTagToPost, function(post) {
+    if (post !== undefined) {
+      bitly.shorten(post.href, function(err, response) {
+        if (err) console.log(err); // TODO: hande better
+        
+        var tweet = formattedTweetWithPostAndURL(post, response.data.url);
+        T.post('statuses/update', { status: tweet }, function(err, data, response){
+          if (err) console.log(err); // TODO: hande better
+          pinboardDataStore.updatePostWithHasBeenTweetedFlag(post);
+          console.log('tweeted!');
+        });
+      });
+    } else {
+      // DM or otherwise notify me that we've tweeted all of the posts for this tag
+    }
+  });
+  setTimeout(tweetRandomPostForCurrentTag, tweetInterval + Math.floor(Math.random() * tweetIntervalVariance));
+}
