176d3ead7d3dd7cb0102799c23dc9ad9e7769b55
Tyler Powers <gr4yscale@gmail.com>
2015-07-19 16:12:13 +0100

    make bot configurable via http through simple express request handlers

---
+++ app.js
@@ -0,0 +1,36 @@
+var TwitterBot = require('./twitter-bot.js');
+var bot = new TwitterBot();
+var tweetInterval = 1000 * 60 * 30; // every 10 mins
+var tweetIntervalVariance = 1000 * 60 * 5;
+
+var express = require('express');
+var app = express();
+
+app.get('/tagsToPost/:tagsToPost', function(req, res) {
+  bot.config.currentTagsToPost = req.params.tagsToPost;
+  res.send('Updated tags to post: ' + bot.config.currentTagsToPost);
+});
+
+app.get('/interval/:interval', function(req, res) {
+  tweetInterval = parseFloat(req.params.interval) * 1000 * 60;
+  res.send('Updated tweet interval: ' + tweetInterval / 1000 / 60);
+  updateInterval();
+});
+
+app.get('/intervalVariance/:intervalVariance', function(req, res) {
+  tweetIntervalVariance = parseFloat(req.params.intervalVariance) * 1000 * 60;
+  res.send('Updated tweet interval variance: ' + tweetIntervalVariance / 1000 / 60);
+  updateInterval();
+});
+
+function tweet() {
+  bot.tweetRandomPostForCurrentTag();
+  updateInterval();
+}
+
+function updateInterval() {
+  setTimeout(tweet, tweetInterval + Math.floor(Math.random() * tweetIntervalVariance));
+}
+
+var server = app.listen(3000);
+tweet();
